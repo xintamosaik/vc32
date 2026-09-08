4bf4dfe5d62a556937371f72de9ad07f7a55c75e
= <=>
2026-09-08 14:48:59 +0200

    add templ

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"github.com/a-h/templ"
 )
 
 var name string
@@ -18,54 +19,23 @@ func handleRequestHome(w http.ResponseWriter, r *http.Request) {
 	parsed.Execute(w, template.HTML(templ))
 }
 
-func handleRequestHello(w http.ResponseWriter, r *http.Request) {
-	templ := "<h1>Hello, {{.}}</h1>" + homeLink
-
-	parsed := template.Must(template.New("hello").Parse(templ))
-	parsed.Execute(w, name)
-}
-
+ 
 func handleRequestNew(w http.ResponseWriter, r *http.Request) {
 	templ := "<h1>New</h1>" + homeLink
 	parsed := template.Must(template.New("new").Parse(templ))
 	parsed.Execute(w, nil)
 }
 
-func handleRequestUpdateUserName(w http.ResponseWriter, r *http.Request) {
-	// check if the request method is POST
-	if r.Method == http.MethodPost {
-		r.ParseForm()
-		newName := r.FormValue("username")
-		if newName != "" {
-			name = newName
-		}
-		http.Redirect(w, r, "/", http.StatusSeeOther)
-	}
-}
-
-func handleRequestEditUserName(w http.ResponseWriter, r *http.Request) {
-	templ := `
-	<h1>Change Name</h1>
-	<form action="/username" method="POST">
-	<label for="username">Name</label>
-		<input name="username" id="username" type="text" /> 
-	</form>
-	` + homeLink
-
-	parsed, err := template.New("hello").Parse(templ)
-	if (err !=nil) {
-		fmt.Fprint(w, "no")
-	}
-	parsed.Execute(w, name)
-}
+ 
 
 func main() {
 	name = "John Doe"
 
 	http.HandleFunc("/", handleRequestHome)
-	http.HandleFunc("/hello", handleRequestHello)
+
 	http.HandleFunc("/new", handleRequestNew)
-	http.HandleFunc("GET /username", handleRequestEditUserName)
+	
+	http.Handle("GET /username", templ.Handler(usernameEdit()))
 	http.HandleFunc("POST /username", handleRequestUpdateUserName)
 
 	http.ListenAndServe(":8080", nil)
